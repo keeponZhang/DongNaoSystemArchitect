90398d484b5acb9991b59a46e74b28b3c457cd0f
keeponZhang <462789909@qq.com>
2019-05-08 22:55:18 +0800

    策略模式

---
+++ designpattern/src/main/java/com/zhang/designpattern/iterator/Aggregate.java
@@ -0,0 +1,13 @@
+package com.dongnao.iterator;
+/**
+ * 容器的接口
+ * @author Administrator
+ */
+public interface Aggregate<T> {
+	void  add(T  t);
+	
+	void remove(T t);
+	
+	Iterator<T> iterator();
+	
+}
---
+++ designpattern/src/main/java/com/zhang/designpattern/stategy/Strategy.java
@@ -0,0 +1,12 @@
+package com.zhang.designpattern.stategy;
+
+/**
+ * @创建者 keepon
+ * @创建时间 2019/4/22 0022 上午 11:19
+ * @描述 ${TODO}
+ * @版本 $$Rev$$
+ * @更新者 $$Author$$
+ * @更新时间 $$Date$$
+ */
+public interface StrategyIngerface {
+}
